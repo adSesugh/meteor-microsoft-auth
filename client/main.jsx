10c324edccd7019648074273419127e4b3412b3b
Mike King <mikkelking@hotmail.com>
2023-06-06 18:55:38 +1000

    Use settings.json

--- client/main.jsx
+++ client/main.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Meteor } from 'meteor/meteor';
-import { App } from '/imports/ui/App';
-import { MsalProvider } from '@azure/msal-react';
-import { PublicClientApplication } from '@azure/msal-browser';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Meteor } from "meteor/meteor";
+import { App } from "/imports/ui/App";
+import { MsalProvider } from "@azure/msal-react";
+import { PublicClientApplication } from "@azure/msal-browser";
 
 Meteor.startup(() => {
-  const container = document.getElementById('react-target');
+  const container = document.getElementById("react-target");
   const root = createRoot(container);
+  const auth = {
+    clientId: Meteor.settings.public.clientId,
+    authority: Meteor.settings.public.authority,
+    redirectUri: Meteor.settings.public.redirectUri,
+    postLogoutRedirectUri: "/", //Meteor.settings.public.REDIRECT_URI
+  };
+  console.log({ auth });
   const pca = new PublicClientApplication({
-    auth: {
-      clientId: "9cea83bd-5e05-448e-a1f3-359e4cdf1536", //process.env.CLIENT_ID,
-      authority: "https://login.microsoftonline.com/b69f83a4-8adc-425a-b475-b2d10fe7f655", // process.env.AUTHORITY,
-      redirectUri: "/", //process.env.REDIRECT_URI,
-      postLogoutRedirectUri: "/" //process.env.REDIRECT_URI
-    }
-  })
-  root.render(<MsalProvider instance={pca}><App /></MsalProvider>);
+    auth,
+  });
+  root.render(
+    <MsalProvider instance={pca}>
+      <App />
+    </MsalProvider>
+  );
 });
